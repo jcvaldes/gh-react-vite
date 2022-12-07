8e6cf58f85cd1e72f5a9f29e0c42018d308e97c7
Juan Carlos Valdés <idevkingos@gmail.com>
2022-12-06 22:34:47 -0300

    fix: bugs

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://jcvaldes.github.io/gh-react-vite',
+  base: 'https://jcvaldes.github.io/gh-react-vite/',
   plugins: [react()]
 })
